1474a00b841eb613d96d8fd67700705232b11438
jtschams <jtschams@yahoo.com>
2024-05-06 19:45:51 -0500

    linked server to lib/index.  added choices reset and extra actions option

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const { handler } = require('./lib');
+
+const PORT = 3001;
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true}));
+
+app.use(async function(req, res, next) {
+    try {
+        handler() 
+    } catch (error) { next(error) }
+
+});
+
+app.listen(PORT, () =>
+    console.log(`App listening at http://localhost:${PORT}`)
+  );
+  
